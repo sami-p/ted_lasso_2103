98c4998ad4d4b052dd4852975eab4f1380167853
sami-p <sami.peterson14@gmail.com>
2021-04-07 10:21:33 -0500

    Fix salary instance variable

--- lib/player.rb
+++ lib/player.rb
@@ -2,10 +2,10 @@ class Player
   attr_reader :name,
               :position,
               :salary
-              
+
   def initialize(player_data)
     @name = player_data[:name]
     @position = player_data[:position]
-    @salaray = player_data[:salary]
+    @salary = player_data[:salary]
   end
 end
