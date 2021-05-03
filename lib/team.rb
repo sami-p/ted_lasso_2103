2f1db7db199cc29aaa9047febaefa47ffc040771
sami-p <sami.peterson14@gmail.com>
2021-05-03 09:41:18 -0500

    Initialize Team with coach

--- lib/team.rb
+++ lib/team.rb
@@ -1,7 +1,9 @@
 class Team
-  attr_reader :name
+  attr_reader :name,
+              :coach
 
   def initialize(name, coach, players)
     @name = name
+    @coach = coach
   end
 end
--- spec/team_spec.rb
+++ spec/team_spec.rb
@@ -19,5 +19,13 @@ RSpec.describe Team do
 
       expect(richmond.name).to eq("AFC Richmond")
     end
+
+    it 'has a coach' do
+      roy = Player.new({name: "Roy Kent", position: "Center Midfielder" , salary: 1_000_000})
+      sam = Player.new({name: "Sam Obisanya", position: "Right-back Defender", salary: 600_000})
+      richmond = Team.new("AFC Richmond", "Ted Lasso", [roy, sam])
+
+      expect(richmond.coach).to eq("Ted Lasso")
+    end
   end
 end
